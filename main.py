44e2191feb8eb76bd0ff072522d9a8125d17005d
xYuKiZ <loginallemail@gmail.com>
2025-01-29 15:46:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+sum = 0
+n = int(input("Enter Number Of Students: "))
+for i in range(0,n):
+    name = int(input("Enter Marks: "))
+    sum = sum + name
+print("Total Marks: %d"%(sum))
+print("Mean: %.2f"%(sum/40))
+
+    
